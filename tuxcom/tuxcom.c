731ea3dfcdf187a312e47100c0b9e0b5e79d5537
BPanther <bpanther_ts@hotmail.com>
2025-10-13 10:46:48 +0200

    dm800: fix tuxcom

--- tuxcom/tuxcom.c
+++ tuxcom/tuxcom.c
@@ -1969,7 +1969,7 @@ void RenderMenuLine(int highlight, int refresh)
 		RenderBox( (viewx/COLORBUTTONS) *i ,viewy-MENUSIZE/2, (viewx/COLORBUTTONS) *(i+1) , viewy , FILL,  (i == 0 ? RED    :
 		                                                            					                   (i == 1 ? GREEN  :
 		                                                            					                   (i == 2 ? YELLOW : BLUE1))));
-#if !BOXMODEL_DCUBE // 3rd value overflow?!
+#if !BOXMODEL_DCUBE && !BOXMODEL_DM800 // 3rd value overflow?!
 		RenderBox( (viewx/COLORBUTTONS) *i ,viewy-MENUSIZE/2, (i < COLORBUTTONS-1 ? (viewx/COLORBUTTONS) *(i+1) : viewx) , viewy , GRID,  WHITE );
 #endif
 #ifdef MARTII
